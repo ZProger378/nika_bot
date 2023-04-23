97ee2501a69fc062fc27ab730a4ffca227cedb0c
ZProger378 <hackm378@gmail.com>
2023-04-23 21:16:42 +0300

    Улучшил распознавание команд и реализовал распознавание нескольких вопросов в одном сообщении

--- fuzz_methods.py
+++ fuzz_methods.py
@@ -1,26 +1,32 @@
 import json
 from random import choice
 from fuzzywuzzy import fuzz
+from fuzzywuzzy.process import extractOne, dedupe
 
 
 def ratio(text, identity):
     # Получаю результаты нечёткого сравнения числом от 1 до 100
-    a = fuzz.WRatio(text, identity)
-    b = fuzz.token_sort_ratio(text, identity)
-    if text in identity:
-        c = 100
-    else:
-        c = 0
+    # a = fuzz.WRatio(text, identity)
+    # b = fuzz.token_sort_ratio(text, identity)
+    # if text in identity:
+    #     c = 100
+    # else:
+    #     c = 0
     # Получаю среднее арифметическое с 2 результатов
-    res = round((a + b + c) / 3)
+    # res = round((a + b + c) / 3)
+    a = extractOne(identity, text)[1]
+    b = fuzz.WRatio(text, identity)
+    с = fuzz.token_set_ratio(text, identity)
+    res = round((a + b + с) / 3)
 
     return res  # Возвращаю результат
 
 
 def get_reply_text(text):
     model = json.load(open("model.json", "rb"))  # Загружаю модель диалога
+    var = model['var']  # Загрузка локальных переменных
     dialog = model['dialog'].keys()  # Получаю все пока изученные вопросы
-    comparisons = []
+    comparisons = []  # Инициализация списка сходств
     extra_chars = "!@#$%^&*()\"'/\\.,?_-=+`"  # "Лишние" символы, которые не стоит считать
     for char in extra_chars: text = text.replace(char, "")  # Очистка текста от "лишних" символов
     # Сравниваю текст, полученный от пользователя со всеми изученными вопросами
@@ -30,19 +36,38 @@ def get_reply_text(text):
             res = ratio(text.lower(), rp)
             comparisons.append([rep, res])
     # Нахожу наибольшее совпадение
-    question = ["!notunderstandme", 100]
+    questions = [["!notunderstandme", 100]]
     for comparison in comparisons:
-        if comparison[1] >= 50:
-            if question == ["!notunderstandme", 100]:
-                question = comparison
+        if comparison[1] >= 70:  # Если совпадение более 70%
+            if questions[0][0] == "!notunderstandme":
+                questions = [comparison]
             else:
-                if comparison[1] > question[1]:
-                    question = comparison
+                questions.append(comparison)
     # Получаю все возможные ответы на вопрос
-    reply = model['dialog'][question[0]]
+    replys = []
+    results = []
+    # Получение текста вопросов
+    qs = []
+    for question in questions:
+        if question[0] not in qs:
+            qs.append(question[0])
+    # Получение ответов
+    for question in qs:
+        replys.append(model['dialog'][question])
+    # Сборка всех ответов
+    for reply in replys:
+        reply = choice(reply)  # Получение случайного ответа на вопрос
+        # Замена имен переменных на их значение из словаря var{}
+        # Пример: "{name}" -> "Неко"
+        for key in var.keys():
+            reply = reply.replace("{" + key + "}", var[key])
+        # Добавление в список ответов
+        results.append(reply)
 
-    return choice(reply)  # Возвращаю случайный ответ
+    return list(dedupe(results))  # Возвращаю ответы
 
 
 while True:
-    print("[NEKO] " + get_reply_text(input("[ME] ")))
+    replys = get_reply_text(input("[ME] "))
+    for reply in replys:
+        print("[NEKO] " + reply)
