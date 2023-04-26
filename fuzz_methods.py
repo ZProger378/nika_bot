3b2a61a0de33ab22799892f427c430e05a9828dc
netrabbit <hackm378@gmail.com>
2023-04-26 21:47:10 +0300

    Собрал бота для на основе готового алгоритма

--- fuzz_methods.py
+++ fuzz_methods.py
@@ -4,20 +4,26 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy.process import extractOne, dedupe
 
 
+# Функция для проверки на то, обращается ли пользователь к боту
+def is_message_to_bot(message_text):
+    # Загрузка всех возможных кличек бота
+    model = json.load(open("model.json", "rb"))
+    names = model['name']
+    # Перебор кличек и проверка
+    for name in names:
+        if message_text.startswith(name):  # Пользователь обращается к боту
+            return True
+    else:
+        return False  # Пользователь не обращается к боту
+
+
 def ratio(text, identity):
     # Получаю результаты нечёткого сравнения числом от 1 до 100
-    # a = fuzz.WRatio(text, identity)
-    # b = fuzz.token_sort_ratio(text, identity)
-    # if text in identity:
-    #     c = 100
-    # else:
-    #     c = 0
-    # Получаю среднее арифметическое с 2 результатов
-    # res = round((a + b + c) / 3)
     a = extractOne(identity, text)[1]
     b = fuzz.WRatio(text, identity)
-    с = fuzz.token_set_ratio(text, identity)
-    res = round((a + b + с) / 3)
+    c = fuzz.token_set_ratio(text, identity)
+    # Получаю среднее арифметическое с3 2 результатов
+    res = round((a + b + c) / 3)
 
     return res  # Возвращаю результат
 
@@ -25,6 +31,7 @@ def ratio(text, identity):
 def get_reply_text(text):
     model = json.load(open("model.json", "rb"))  # Загружаю модель диалога
     var = model['var']  # Загрузка локальных переменных
+    names = model['name']  # Загрузка кличек бота
     dialog = model['dialog'].keys()  # Получаю все пока изученные вопросы
     comparisons = []  # Инициализация списка сходств
     extra_chars = "!@#$%^&*()\"'/\\.,?_-=+`"  # "Лишние" символы, которые не стоит считать
@@ -36,17 +43,21 @@ def get_reply_text(text):
             res = ratio(text.lower(), rp)
             comparisons.append([rep, res])
     # Нахожу наибольшее совпадение
-    questions = [["!notunderstandme", 100]]
-    for comparison in comparisons:
-        if comparison[1] >= 70:  # Если совпадение более 70%
-            if questions[0][0] == "!notunderstandme":
-                questions = [comparison]
-            else:
-                questions.append(comparison)
+    if text in names:  # Если сообщение представляет собой просто обращение к боту
+        questions = [["!call_bot", 100]]
+    else:  # Иначе
+        questions = [["!notunderstandme", 100]]
+        for comparison in comparisons:
+            if comparison[1] >= 70:  # Если совпадение более 70%
+                # Добавляем распознанную команду в список
+                if questions[0][0] == "!notunderstandme":
+                    questions = [comparison]
+                else:
+                    questions.append(comparison)
     # Получаю все возможные ответы на вопрос
     replys = []
     results = []
-    # Получение текста вопросов
+    # Получение вопросов
     qs = []
     for question in questions:
         if question[0] not in qs:
@@ -64,10 +75,7 @@ def get_reply_text(text):
         # Добавление в список ответов
         results.append(reply)
 
-    return list(dedupe(results))  # Возвращаю ответы
+    return list(dedupe(results))  # Возвращаю ответы в виде списка
 
 
-while True:
-    replys = get_reply_text(input("[ME] "))
-    for reply in replys:
-        print("[NEKO] " + reply)
+print("[i] FUZZ_METHODS is loaded!")
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import json
 from config import bot
+import fuzz_methods
+from fuzz_methods import is_message_to_bot
 
 
 # обработчик команды /start
@@ -7,7 +10,20 @@ def start_handler(message):
     bot.send_message(message.chat.id, f"Привет, {message.chat.first_name}")
 
 
+@bot.message_handler(chat_types=["group", "supergroup"])
+def main_handler(message):
+    text = message.text.lower()  # Преобразуем текст в lowercase
+    if is_message_to_bot(text):  # Если обращаются к боту
+        answers = fuzz_methods.get_reply_text(text)  # Получаю ответ на команды
+        # Отправка сообщений
+        for i, answer in enumerate(answers):
+            if not i:
+                bot.reply_to(message, answer)
+            else:
+                bot.send_message(message.chat.id, answer)
+
+
 if __name__ == "__main__":
-    # запуск бота методом .polling()
+    # Запуск бота методом .polling()
     print("[i] Bot is started...")
     bot.polling(none_stop=True, interval=0)
